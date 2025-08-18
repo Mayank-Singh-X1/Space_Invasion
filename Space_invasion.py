e94b08a06e54a2f43e3cce3e9b1bb9547de65744
Mayank-Singh <mayankbrawler@gmail.com>
2025-08-18 16:56:05 +0530

    added caption and icon

--- Space_invasion.py
+++ Space_invasion.py
@@ -1,10 +1,18 @@
 import pygame
 pygame.init()
 
+#Screen
 screen=pygame.display.set_mode((800,600))
+
+#title
+pygame.display.set_caption("Space Invader")
+
+#icon
+icon=pygame.image.load('spaceship.webp')
+pygame.display.set_icon(icon)
 
 running=True
 while running:
-    for event in pygame.event.get():
+    for event in pygame.event.get(): 
         if event.type==pygame.QUIT:
             running=False
